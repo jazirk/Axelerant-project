a4b6375c259703c295a157489f713d4af051dbec
jazirk <dev.jasirk@gmail.com>
2021-01-25 02:01:28 +0530

    new file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoteListComponent } from './pages/note-list/note-list.component';
-import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
-import { NoteCardComponent } from './components/note-card/note-card.component';
-import { HomeComponent } from './core/components/home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NoteListComponent,
-    MainLayoutComponent,
-    NoteCardComponent,
-    HomeComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]

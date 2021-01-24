5182c61f8a6933a7b016e250627e7b1536ffdfb3
jazirk <dev.jasirk@gmail.com>
2021-01-25 01:54:19 +0530

    added file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './components/note-card/note-card.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent,
+    MainLayoutComponent,
+    NoteCardComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

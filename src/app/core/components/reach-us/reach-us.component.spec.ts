5d9f722491d465c4e4149271e5e5decc74509425
jazir-mubarak <jasirk@infrrd.ai>
2021-01-29 01:36:15 +0530

    changes

---
+++ src/app/core/components/reach-us/reach-us.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReachUsComponent } from './reach-us.component';
+
+describe('ReachUsComponent', () => {
+  let component: ReachUsComponent;
+  let fixture: ComponentFixture<ReachUsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReachUsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReachUsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
